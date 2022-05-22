b463de2322a1bc797d6d9c539be8bce31ece4138
Vitalii <sv.legion8@gmail.com>
2022-05-22 17:33:28 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallaryContainer = document.querySelector('.gallery');
+const createGallaryImages = createGalleryImagesMarcup(galleryItems);
+
+gallaryContainer.insertAdjacentHTML('afterbegin', createGallaryImages);
+gallaryContainer.addEventListener('click', onClickImages);
+
+function createGalleryImagesMarcup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => 
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" onclick = "event.preventDefault()">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+).join('');
+}
+
+function onClickImages(e) {
+  gallaryContainer.addEventListener('keydown', (e) => {
+ if (e.code ==="Escape") {
+  image.close();
+}
+  });
+  e.preventDefault(); 
+  
+ const image = basicLightbox.create(`
+  <img src="${e.target.dataset.source}"/>`
+);
+if (!e.target.classList.contains("gallery__image")) {
+  return;
+} else {
+    image.show();
+  }
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallaryContainer = document.querySelector('.gallery');
+const createGallaryImages = createGalleryImagesMarkup(galleryItems);
+
+gallaryContainer.insertAdjacentHTML('afterbegin', createGallaryImages);
+
+function createGalleryImagesMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"  
+    />
+  </a>`
+).join('');
+}
+
+new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt" });
+
 console.log(galleryItems);
